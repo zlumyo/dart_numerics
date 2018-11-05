ed76d236ca07167e9f09bf82d7fba8d06fb59b8d
Vladimir Ivanov <zlumyo@gmail.com>
2018-11-05 14:45:58 +0400

    Fixed missed export

--- lib/dart_numerics.dart
+++ lib/dart_numerics.dart
@@ -4,6 +4,7 @@
 library dart_numerics;
 
 export 'src/constants.dart';
+export 'src/permutation.dart';
 export 'src/precision.dart';
 export 'src/precision_equality.dart';
 export 'src/trigonometry.dart';
