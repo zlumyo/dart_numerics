a54fbd7c060f76828424ee5ec13414d4f9052bf4
Vladimir Ivanov <zlumyo@gmail.com>
2018-11-06 09:33:20 +0400

    Add to all tests executor missed unit

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,5 +1,6 @@
 import 'package:test/test.dart';
 
+import 'fit_test.dart' as fit;
 import 'permutation_test.dart' as permutation;
 import 'precision_test.dart' as precision;
 import 'trigonometry_test.dart' as trigonometry;
@@ -8,4 +9,5 @@ void main() {
   group('permutation', permutation.main);
   group('precision', precision.main);
   group('trigonometry', trigonometry.main);
+  group('Fit tests', fit.main);
 }
