446835cbe38eed10d41e1494247f9dbcbba1fb24
Vladimir Ivanov <zlumyo@gmail.com>
2018-11-07 09:45:23 +0400

    Fixed missed export of Beta functions

--- lib/dart_numerics.dart
+++ lib/dart_numerics.dart
@@ -18,6 +18,7 @@
 library dart_numerics;
 
 export 'src/linear_regression/simple_regression.dart';
+export 'src/special_functions/beta.dart';
 export 'src/special_functions/gamma.dart';
 export 'src/constants.dart';
 export 'src/euclid.dart';
